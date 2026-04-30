82f9d422ae11c0dc7ebf707bf2fcdb14433a35b0
Walter Gugenberger <wuiidl@users.noreply.github.com>
2026-04-30 08:53:44 -0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s == null || t == null) return false;
+        if(s.length() != t.length()) return false;
+
+        int[] chars = new int[26];
+
+        for(char c : s.toCharArray()) {
+            chars[c - 'a']++;
+        }
+
+        for(char c : t.toCharArray()) {
+            chars[c - 'a']--;
+        }
+
+        for(int i : chars){
+            if(i != 0) return false;
+        }
+        return true;
+    }
+}
