284926479a2732eedaaa2262ae62c0003ecfaae4
Walter Gugenberger <wuiidl@users.noreply.github.com>
2026-05-06 09:51:40 -0400

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.java
@@ -0,0 +1,57 @@
+class Solution {
+
+    class TrieNode {
+        Map<Character, TrieNode> children = new HashMap<>();
+
+        public TrieNode(){}
+
+    }
+
+    class Trie {
+        TrieNode root = new TrieNode();
+
+        void insert(String word) {
+            TrieNode head = root;
+            for(char c : word.toCharArray()) {
+                head.children.putIfAbsent(c, new TrieNode());
+                head = head.children.get(c);
+            }
+        }
+
+        int lcp(String word, int pl) {
+            TrieNode node = root;
+            int i = 0;
+            while(i < Math.min(word.length(), pl)) {
+                if(!node.children.containsKey(word.charAt(i))) {
+                    return i;
+                }
+                node = node.children.get(word.charAt(i));
+                i++;
+            }
+            return Math.min(word.length(), pl);
+        }
+    }
+
+
+
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 1) return strs[0];
+
+        // find smallest one
+        int mini = 0;
+        for(int i = 1; i < strs.length; i++) {
+            if(strs[mini].length() > strs[i].length()) {
+                mini = i;
+            }
+        }
+
+        Trie trie = new Trie();
+        trie.insert(strs[mini]);
+        int prefixLen = strs[mini].length();
+
+        for(int i = 0; i < strs.length;i++) {
+            prefixLen = trie.lcp(strs[i], prefixLen);
+        }
+        return strs[0].substring(0, prefixLen);
+    }
+}
