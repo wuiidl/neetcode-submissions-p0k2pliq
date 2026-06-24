8e3bc1b4d4af8cd334a44228ceeba282b0174216
Walter Gugenberger <wuiidl@users.noreply.github.com>
2026-06-24 10:53:51 -0400

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        if(s.length() <= 1) return true;
+
+        char[] c = s.toLowerCase().replaceAll("[^a-z0-9]", "").toCharArray();
+        int n = c.length;
+        int l = 0;
+        int r = n - 1;
+
+        while(l <= r) {
+            if(c[l] != c[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
